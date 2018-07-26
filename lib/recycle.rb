bcb30a76f13bd94ea7fd22b18f1f1c99dd8a51f2
ziesop20 <sincerelysophie@hotmail.com>
2018-07-26 16:45:59 +0000

    automatically backed up by learn

--- lib/recycle.rb
+++ lib/recycle.rb
@@ -4,7 +4,7 @@
 #The specs (which call your tests) are using these exact hashes 
 # We haven't hard coded the hashes in the tests, 
 #they're dynamically brought in from the NYC Open Data API
-def recycling_info
+
 first_recycle_bin =  {
   "address"=>"E 227 St/Bronx River Pkway",
   "site_type"=>"Subproperty",
